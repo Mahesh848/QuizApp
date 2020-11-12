80ef0e0f12d29d6a69816c438b66526ab8b21e01
Mahesh Ippili <maheshippili@Maheshs-MacBook-Pro.local>
2020-11-12 11:45:09 +0530

    setup project

---
+++ src/components/Entry.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Entry extends React.Component {
+    render() {
+        return (
+            <div className="login-signup-page">
+                <h1>LogIn</h1>
+                <h1>Signup</h1>
+            </div>
+        )
+    }
+}
+export default Entry
